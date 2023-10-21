2ed6f65bb48076d103e95a78ba91071536b77fef
Guillaume Dieudonné <8122069+GuiDieudo@users.noreply.github.com>
2023-10-21 11:02:35 +0200

    fix: cleaning page icons error

--- lib/applications.ts
+++ lib/applications.ts
@@ -1,44 +1,61 @@
 import { applications } from "@/config/tools"
 import { GENERATED_BY_ENDPOINT } from "@/config/constants"
 import { fetcher } from "./fetcher"
+const fetchApplicationData = async (applicationId: string) => {
+  return await fetcher(
+    `${GENERATED_BY_ENDPOINT}/applications/${applicationId}`,
+    {
+      headers: {
+        Authorization: `Bearer ${process.env.GENERATED_BY_API_KEY}`,
+      },
+    }
+  )
+}
 
 export const loadFullApplications = async () => {
-  const fullApplications = await Promise.allSettled(
-    applications.map(async (application) => {
-      const data = await fetcher(
-        `${GENERATED_BY_ENDPOINT}/applications/${application.id}`,
-        {
-          headers: {
-            Authorization: `Bearer ${process.env.GENERATED_BY_API_KEY}`,
-          },
+  try {
+    const fullApplications = await Promise.allSettled(
+      applications.map(async (application) => {
+        const data = await fetchApplicationData(application.id)
+        const fullApplication = {
+          ...application,
+          ...data,
         }
-      )
-      return {
-        ...application,
-        ...data,
-      }
-    })
-  )
 
-  const successfulApplications = fullApplications
-    .filter((result) => result.status === "fulfilled")
-    .map((result) => (result as PromiseFulfilledResult<any>).value)
+        return fullApplication
+      })
+    )
 
-  return successfulApplications
+    const successfulApplications = fullApplications
+      .filter((result) => result.status === "fulfilled")
+      .map((result) => (result as PromiseFulfilledResult<any>).value)
+
+    return successfulApplications
+  } catch (error) {
+    console.error(`Failed to load full applications`, error)
+    return []
+  }
 }
 
 export const loadFullApplication = async (applicationId: string) => {
   try {
-    const application = await fetcher(
-      `${GENERATED_BY_ENDPOINT}/applications/${applicationId}`,
-      {
-        headers: {
-          Authorization: `Bearer ${process.env.GENERATED_BY_API_KEY}`,
-        },
-      }
-    )
+    const application = applications.find((app) => app.id === applicationId)
 
-    return application
+    if (!application) {
+      console.error(
+        `Application with id ${applicationId} not found in applications array`
+      )
+      return null
+    }
+
+    const data = await fetchApplicationData(applicationId)
+
+    const fullApplication = {
+      ...application,
+      ...data,
+    }
+
+    return fullApplication
   } catch (error) {
     console.error(`Failed to load application with id ${applicationId}`, error)
     return null

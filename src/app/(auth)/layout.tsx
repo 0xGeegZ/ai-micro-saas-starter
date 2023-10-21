44e8b8f58f4b20afdf815a874bb4cb1aaed627b3
Guillaume Dieudonné <8122069+GuiDieudo@users.noreply.github.com>
2023-10-21 15:48:08 +0200

    feat: merge with master

--- src/app/(auth)/layout.tsx
+++
@@ -1,9 +0,0 @@
-const AuthLayout = ({ children }: { children: React.ReactNode }) => {
-  return (
-    <main className="h-full bg-background flex items-center justify-center">
-      {children}
-    </main>
-  )
-}
-
-export default AuthLayout
